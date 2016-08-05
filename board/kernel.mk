553edecb62a6474813f310a6e812e59ca86f525e
Arvin Quilao <arquilao@gmail.com>
2016-08-05 10:32:09 +0800

    Initial AOSPA bringup

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,3 +2,5 @@
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_CONFIG := cyanogenmod_seed_defconfig
+# include device/google/common-headers/KernelHeaders.mk
+# TARGET_BOARD_KERNEL_HEADERS := device/google/seed/kernel-headers
--- board/sepolicy.mk
+++ board/sepolicy.mk
@@ -1,3 +1,3 @@
 # SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/google/seed/sepolicy
+# BOARD_SEPOLICY_DIRS += \
+#   device/google/seed/sepolicy
