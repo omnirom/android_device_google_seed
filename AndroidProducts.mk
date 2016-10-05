a35d7ce1a949e6a3d9e9c4348d053ff4efe9f2a9
Arvin Quilao <arvin@arvinquilao.com>
2016-10-05 14:00:31 +0800

    Initial Omni bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/seed.mk \
+    $(LOCAL_DIR)/omni_seed.mk
---
+++ omni_seed.mk
@@ -0,0 +1,54 @@
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for grouper hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
+
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# must be before including omni part
+TARGET_BOOTANIMATION_SIZE := 640x362
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/google/seed/seed.mk)
+
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8916
+
+# Override AOSP build properties
+PRODUCT_NAME := omni_seed
+PRODUCT_DEVICE := seed
+PRODUCT_BRAND := google
+PRODUCT_MODEL := seed
+PRODUCT_MANUFACTURER := google
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=seed \
+    BUILD_FINGERPRINT=google/seed_l8150/seed:7.0/NRD90R/3141966:user/release-keys \
+    PRIVATE_BUILD_DESC="seed_l8150-user 7.0 NRD90R 3141966 release-keys"
+ 
