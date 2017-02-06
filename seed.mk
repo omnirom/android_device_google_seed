db201639478250a371e3ceaaf839e5874742086e
Arvin Quilao <arquilao@gmail.com>
2017-02-06 02:50:25 +0000

    seed: seed-vendor.mk -> device-vendor.mk
    Change-Id: Ia13f7ff8e84c1eee97b707b08322a76da90add75

--- seed.mk
+++ seed.mk
@@ -23,4 +23,4 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Include package config fragments
 include $(LOCAL_PATH)/product/*.mk
 
-$(call inherit-product-if-exists, vendor/google/seed/seed-vendor.mk)
+$(call inherit-product-if-exists, vendor/google/seed/device-vendor.mk)
